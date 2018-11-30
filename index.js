87c05cca6493240796b997e3ae393bc3cc732e83
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2018-11-30 10:58:42 +0100

    Implement the source annotation logic

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-const { parse } = require('@babel/parser')
-const fixture = 'console.log(\'// hello\'); // hello\nvar a = 1; /* \n boohoo */'
+const { parse } = require("@babel/parser");
+const fixture = "console.log('// hello'); // hello\nvar a = 1; /* \n boohoo */";
 
-const ast = parse(
-  fixture
-)
+const ast = parse(fixture);
 
 const { comments } = ast;
 
 const chunkSize = 2;
 
-const commentsPositions = comments.map(({ start, end }) => ({ start, end }))
+const commentsPositions = comments.map(({ start, end }) => ({ start, end }));
 const commentsPositionsLength = commentsPositions.length;
 
-let chunkBoundaries = commentsPositions.reduce((acc, { start, end }) => [...acc, start, end], []);
+// Create an array of breakpoints between comments and code
+let chunkBoundaries = commentsPositions.reduce(
+  (acc, { start, end }) => [...acc, start, end],
+  []
+);
 
 if (chunkBoundaries[0] !== 0) {
-  chunkBoundaries = [0, ...chunkBoundaries]
+  chunkBoundaries = [0, ...chunkBoundaries];
 }
 
-if (chunkBoundaries[chunkBoundaries.length - 1 ] !== fixture.length) {
-  chunkBoundaries = [...chunkBoundaries, fixture.length - 1 ]
+if (chunkBoundaries[chunkBoundaries.length - 1] !== fixture.length) {
+  chunkBoundaries = [...chunkBoundaries, fixture.length - 1];
 }
 
-const chunkStartEndPairs = []
+// Split the breakpoints into positions of chunks.
+let chunkStartEndPairs = [];
 
 for (let i = 0; i < chunkBoundaries.length; i++) {
-  chunkStartEndPairs.push(chunkBoundaries.slice(i, i + 2))
+  chunkStartEndPairs.push(chunkBoundaries.slice(i, i + 2));
 }
 
-console.log(chunkStartEndPairs)
+// Drop the last one if it points to the end of the source.
+if (
+  chunkStartEndPairs[chunkStartEndPairs.length - 1].length === 1 &&
+  chunkStartEndPairs[chunkStartEndPairs.length - 1][0] === fixture.length
+) {
+  chunkStartEndPairs = chunkStartEndPairs.slice(
+    0,
+    chunkStartEndPairs.length - 1
+  );
+}
+
+// Extract the actual code along with the positions and the order.
+const chunkedSource = chunkStartEndPairs.map((pos, i) => {
+  const [x, y] = pos;
+  return { pos, i, source: fixture.slice(x, y) };
+});
+
+// Figure out whether it's code or a comment
+const annotatedChunks =   chunkedSource.map(block => {
+  const [x, y] = block.pos;
+  let type = 'code'
+
+  if (commentsPositions.find(({ start, end }) => start === x && end === y)) {
+    type = 'comment'
+  }
+
+  return { ...block, type }
+});
+
+console.log(
+  annotatedChunks
+);
